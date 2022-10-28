586871edb8df44a23f2899305d7a79b1931791fd
aCute5 <costantini95dario@gmail.com>
2022-10-28 17:19:23 +0200

    added- palindrome

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// Creo una constante dove chiedo all'utente la sua parola. 
+// Creo una costante che corrisponde alla parola data divisa in stringhe
+// Prendo le stringhe della parole e le inverto. 
+
+
+
+
+const userWord = prompt("Qual è la tua parola?");
+const userLetters = userWord.split("")
+const lettersReverse = userLetters.reverse("")
